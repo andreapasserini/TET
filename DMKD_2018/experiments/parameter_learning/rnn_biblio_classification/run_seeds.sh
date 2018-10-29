6d556567f93faf73cd1266b85125461c27c0fdc8
Giovanni Pellegrini <pellegrini@lion0B.science.unitn.it>
2018-10-29 15:37:52 +0100

    Updated run files with seeds

--- DMKD_2018/experiments/parameter_learning/rnn_biblio_classification/run_seeds.sh
+++ DMKD_2018/experiments/parameter_learning/rnn_biblio_classification/run_seeds.sh
@@ -1,4 +1,10 @@
+mkdir -p logs
+mkdir -p logs/full logs/reduced
 for s in 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181 6765 10946
 do
    ./run_classification_local.sh tet-def data/full/dataset.train data/full/dataset.test data/full/labels.train data/full/labels.test logs/$s\_cl.log $s 
+done
+for s in 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181 6765 10946
+do
+   ./run_classification_local.sh tet-def data/reduced/dataset.train data/reduced/dataset.test data/reduced/labels.train data/reduced/labels.test logs/$s\_cl.log $s 
 done
--- DMKD_2018/experiments/parameter_learning/rnn_biblio_regression/run_seeds.sh
+++ DMKD_2018/experiments/parameter_learning/rnn_biblio_regression/run_seeds.sh
@@ -1,9 +1,17 @@
+mkdir -p logs
+mkdir -p logs/full logs/reduced
 for i in 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181 6765 10946
 do 
-    ./run_regression_local.sh tet-def data/full/dataset.train data/full/dataset.test data/full/eindex.train data/full/eindex.test logs/eindex_$i.log $i
-    ./run_regression_local.sh tet-def data/full/dataset.train data/full/dataset.test data/full/gindex.train data/full/gindex.test logs/gindex_$i.log $i
-    ./run_regression_local.sh tet-def data/full/dataset.train data/full/dataset.test data/full/hindex.train data/full/hindex.test logs/hindex_$i.log $i
-    ./run_regression_local.sh tet-def data/full/dataset.train data/full/dataset.test data/full/i10index.train data/full/i10index.test logs/i10index_$i.log $i
+    ./run_regression_local.sh tet-def data/full/dataset.train data/full/dataset.test data/full/eindex.train data/full/eindex.test logs/full/eindex_$i.log $i
+    ./run_regression_local.sh tet-def data/full/dataset.train data/full/dataset.test data/full/gindex.train data/full/gindex.test logs/full/gindex_$i.log $i
+    ./run_regression_local.sh tet-def data/full/dataset.train data/full/dataset.test data/full/hindex.train data/full/hindex.test logs/full/hindex_$i.log $i
+    ./run_regression_local.sh tet-def data/full/dataset.train data/full/dataset.test data/full/i10index.train data/full/i10index.test logs/full/i10index_$i.log $i
 done
-
+for i in 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181 6765 10946
+do 
+    ./run_regression_local.sh tet-def data/reduced/dataset.train data/reduced/dataset.test data/reduced/eindex.train data/reduced/eindex.test logs/reduced/eindex_$i.log $i
+    ./run_regression_local.sh tet-def data/reduced/dataset.train data/reduced/dataset.test data/reduced/gindex.train data/reduced/gindex.test logs/reduced/gindex_$i.log $i
+    ./run_regression_local.sh tet-def data/reduced/dataset.train data/reduced/dataset.test data/reduced/hindex.train data/reduced/hindex.test logs/reduced/hindex_$i.log $i
+    ./run_regression_local.sh tet-def data/reduced/dataset.train data/reduced/dataset.test data/reduced/i10index.train data/reduced/i10index.test logs/reduced/i10index_$i.log $i
+done
 
